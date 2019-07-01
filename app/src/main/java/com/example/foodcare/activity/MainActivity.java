c59858ecb8d19f4975a45132381864ac2a974872
MackHsu <786188853@qq.com>
2019-07-01 08:52:59 +0800

    第一次合并

--- app/src/main/java/com/example/foodcare/activity/MainActivity.java
+++ app/src/main/java/com/example/foodcare/activity/MainActivity.java
@@ -3,8 +3,8 @@
 //使用github上的CircleTextImageView来做圆形头像框 https://github.com/CoolThink/CircleTextImageView
 package com.example.foodcare.activity;
 
-import android.app.Dialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -14,11 +14,13 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.SearchView;
 
 import com.example.foodcare.R;
 import com.example.foodcare.adapter.MainRecyclerAdapter;
 import com.example.foodcare.entity.MainFood;
 import com.example.foodcare.entity.MainGroup;
+import com.thinkcool.circletextimageview.CircleTextImageView;
 
 import java.util.ArrayList;
 
@@ -28,9 +30,13 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout mainDrawerLayout;
     ArrayList<MainGroup> groupList;
     Button addFoodButton;
-
+    Button analysisButton;
+    Button calendarButton;
+    ImageButton cameraButton;
+    CircleTextImageView UserInformation;
     RecyclerView mainRecycler;
-
+    SearchView searchView;
+private  Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +49,22 @@ public class MainActivity extends AppCompatActivity {
         menuButton = (ImageButton) findViewById(R.id.main_menu_button);
         mainDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         addFoodButton = (Button) findViewById(R.id.mian_add_button);
+        analysisButton = (Button) findViewById(R.id.main_analysis_button);
+        calendarButton = (Button) findViewById(R.id.calendar) ;
+        UserInformation = (CircleTextImageView) findViewById(R.id.avatar);
         mainRecycler = (RecyclerView) findViewById(R.id.main_recycler);
+        cameraButton = (ImageButton)findViewById(R.id.main_camera_button);
+        searchView = (SearchView)findViewById(R.id.mainsearchView);
 
+
+        //点击左上方的按钮左侧菜单栏滑出
         menuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mainDrawerLayout.openDrawer(GravityCompat.START);
             }
         });
+        //点击添加跳转到添加饮食情况界面
         addFoodButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,11 +72,104 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        //点击左侧界面中的用户头像跳转到用户详细信息界面
+        UserInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UserInfoActivity.class);
+                startActivity(intent);
+            }
+        });
+        //搜索框点击跳转到搜索界面
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        //点击相机图片进入照相界面
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+/*
+                final DialogPlus dialog = DialogPlus.newDialog(MainActivity.this)
+                        .setContentHolder(new ViewHolder(R.layout.dialog_items))
+                        .create();
+                Button buttonTake=(Button)dialog.findViewById(R.id.take_photo_item_dialog);
+                Button buttonSelect=(Button)dialog.findViewById(R.id.select_photo_item_dialog);
+                buttonTake.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        File outputImage=new File(getExternalCacheDir(),"output_image.jpg");
+                        try{
+                            if(outputImage.exists()){
+                                outputImage.delete();
+                            }
+                            outputImage.createNewFile();
+                        }
+                        catch (IOException e){
+                            e.printStackTrace();
+                        }
+                        if(Build.VERSION.SDK_INT>=24){
+                            try{
+                                imageUri= FileProvider.getUriForFile(MainActivity.this,"com.example.search.fileprovider",outputImage);
+                            }
+                            catch (Exception e){
+                                Toast.makeText(MainActivity.this,"faile to get image ",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else{
+                            imageUri= Uri.fromFile(outputImage);
+                        }
+                        Intent intent=new Intent("android.media.action.IMAGE_CAPTURE");
+                        intent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                        startActivityForResult(intent,TAKE_PHOTO);
+                        dialog.dismiss();
+                    }
+                });
+                buttonSelect.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                        }
+                        else{
+                            openAlbum();
+
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+           */ }
+        });
+
 
         //显示列表
         mainRecycler.setLayoutManager(new LinearLayoutManager(this));
         MainRecyclerAdapter adapter = new MainRecyclerAdapter(this,groupList);
         mainRecycler.setAdapter(adapter);
+
+        //日历跳转
+        //日历界面跳转
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalendarActivity.class);
+                startActivity(intent);
+            }
+        });
+        //添加食物跳转
+        analysisButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TodayAnalyseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -88,4 +195,7 @@ public class MainActivity extends AppCompatActivity {
         mainGroup = new MainGroup("午餐", 450, foodList);
         groupList.add(mainGroup);
     }
+    /*********************************************************************/
+    /**********************搜索框*****************************************/
+
 }
