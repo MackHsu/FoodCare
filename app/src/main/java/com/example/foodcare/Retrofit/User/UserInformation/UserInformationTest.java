5fd65e50c4b04fee962ff14cee9a217ba61c3459
blaveer <1914481614@qq.com>
2019-07-08 19:06:04 +0800

    完备了维护用户信息的功能

--- app/src/main/java/com/example/foodcare/Retrofit/User/UserInformation/UserInformationTest.java
+++ app/src/main/java/com/example/foodcare/Retrofit/User/UserInformation/UserInformationTest.java
@@ -1,6 +1,9 @@
 package com.example.foodcare.Retrofit.User.UserInformation;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.Toast;
 
@@ -15,8 +18,12 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class UserInformationTest {
-    static Account account;
-    static public void request(int id,final Context context) {
+    public Account account;
+    public final int ACCOUNT_GET_SUCCESS=8;
+    public  final int ACCOUNT_GET_FAILE=9;
+    public Handler handler;
+    public void setHandler(Handler handler){this.handler = handler;}
+    public void request(int id,final Context context) {
         Retrofit retrofit  = new Retrofit.Builder()
                 .baseUrl(IP.ip)
                 .addConverterFactory(new NullOnEmptyConverterFactory())
@@ -32,13 +39,21 @@ public class UserInformationTest {
             @Override
             public void onResponse(Call<Account> call, Response<Account> response) {
                 System.out.println("请求成功");
-                account = response.body();
-                System.out.println(account.getName());
-                String text = "";
-                text = account.getName()+"/n";
-                Toast toast=Toast.makeText(context,text,Toast.LENGTH_SHORT    );
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.show();
+                if(response.body()!=null){
+                    account = response.body();
+                    System.out.println(account.getName());
+                    String text = "";
+                    text = account.getName()+"/n";
+                    Toast toast=Toast.makeText(context,text,Toast.LENGTH_SHORT    );
+                    toast.setGravity(Gravity.CENTER, 0, 0);
+                    toast.show();
+                    Message message = new Message();
+                    message.what = ACCOUNT_GET_SUCCESS;
+                    handler.sendMessage(message);
+                }
+                else{
+                    Log.i("TAG","返回数据为空");
+                }
             }
 
             @Override
--- app/src/main/java/com/example/foodcare/entity/AccountID.java
+++ app/src/main/java/com/example/foodcare/entity/AccountID.java
@@ -1,5 +1,7 @@
 package com.example.foodcare.entity;
 
+import com.example.foodcare.Retrofit.A_entity.Account;
+
 public class AccountID {
     static private int id=-1;
     static public int getId(){return id;}
