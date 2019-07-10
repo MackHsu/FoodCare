ea4694dd7985725f3ccab4b95c0a0f667844eb27
曾志昊 <840047816@qq.com>
2019-07-10 21:01:10 +0800

    基本完成了添加运动的界面

---
+++ app/src/main/java/com/example/foodcare/adapter/SportAdapter.java
@@ -0,0 +1,26 @@
+package com.example.foodcare.adapter;
+
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.example.foodcare.R;
+import com.example.foodcare.Retrofit.A_entity.Sport;
+
+import java.util.List;
+
+public class SportAdapter  extends BaseQuickAdapter<Sport, BaseViewHolder>{
+    public SportAdapter(int layoutResId, List data)
+    {
+        super(layoutResId, data);
+    }
+
+    @Override
+    protected void convert(BaseViewHolder helper, Sport item) {
+        Glide.with(mContext).load(item.getPicUrl()).crossFade().into((ImageView) helper.getView(R.id.sport_image));
+        helper.setText(R.id.sport_name_text, item.getName());//运动名
+        helper.setText(R.id.sport_energy_text, item.getConsume()+"");//运动消耗
+        helper.addOnClickListener(R.id.sport_item_layout);
+    }
+}
