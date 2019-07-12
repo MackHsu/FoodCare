4ed67cf64fcb39d096151b75cd6491a3070bbcbe
blaveer <1914481614@qq.com>
2019-07-12 20:56:44 +0800

    修改了部分颜色

--- app/src/main/java/com/example/foodcare/ToolClass/IP.java
+++ app/src/main/java/com/example/foodcare/ToolClass/IP.java
@@ -1,8 +1,8 @@
 package com.example.foodcare.ToolClass;
 
 public class IP {
-    public static String ip = "http://192.168.137.166:8080/foodcare/";//本地局域网
-//    public static String ip = "http://129.211.79.163:9921/foodcare/";//云端
+    //public static String ip = "http://192.168.137.166:8080/foodcare/";//本地局域网
+    public static String ip = "http://129.211.79.163:9921/foodcare/";//云端
 //    public static String ip = "http://111.230.227.150:8080/foodcare/";//刘宇 云端
 
     //"http://129.211.79.163:9921/foodcare/"; //云端
